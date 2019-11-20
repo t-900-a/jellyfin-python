889da5c4bac4b385cce3516e6c8502518c5977f6
TiedToAStar <TiedToAStar@protonmail.com>
2019-11-19 21:32:43 -0500

    fixed indentation

--- mediaServer/server.py
+++ mediaServer/server.py
@@ -1,7 +1,8 @@
 from mediaServer.userhelper import UserHelper
 from mediaServer.user import User
-#from __init__ import __version__
-__version__= 1
+
+# from __init__ import __version__
+__version__ = 1
 from . import exceptions
 import requests
 import socket
@@ -11,8 +12,8 @@ import pprint
 
 _log = logging.getLogger(__name__)
 
-class MediaServer(object):
 
+class MediaServer(object):
     userHelper = UserHelper()
     url = None
     adminUser = None
@@ -39,14 +40,15 @@ class MediaServer(object):
     def authenticatebyname(self, username, password):
         method = '/Users/AuthenticateByName'
 
-        xEmbyAuth = {'X-Emby-Authorization': 'Emby UserId="{UserId}", Client="{Client}", Device="{Device}", DeviceId="{DeviceId}", Version="{Version}", Token="""'.format(
-            UserId="", # not required, if it was we would have to first request the UserId from the username
-            Client='account-automation',
-            Device=socket.gethostname(),
-            DeviceId=hash(socket.gethostname()),
-            Version=__version__,
-            Token="" # not required
-        )}
+        xEmbyAuth = {
+            'X-Emby-Authorization': 'Emby UserId="{UserId}", Client="{Client}", Device="{Device}", DeviceId="{DeviceId}", Version="{Version}", Token="""'.format(
+                UserId="",  # not required, if it was we would have to first request the UserId from the username
+                Client='account-automation',
+                Device=socket.gethostname(),
+                DeviceId=hash(socket.gethostname()),
+                Version=__version__,
+                Token=""  # not required
+            )}
 
         data = {'Username': username, 'Password': password,
                 'Pw': password}
@@ -63,7 +65,7 @@ class MediaServer(object):
         return self.userHelper.toUserObj(dictUser=dictUser)
 
     def authenticate(self, userid, password):
-        #TODO manual testing then unit testing
+        # TODO manual testing then unit testing
         method = '/Users/{userid}/Authenticate'.format(userid=userid)
         data = {'Pw': password,
                 'Password': password}
@@ -79,10 +81,10 @@ class MediaServer(object):
             if type(e) == exceptions.JellyfinUnauthorized:
                 _log.warning('host: %s userid: %s Authentication Failed' % (self.url, userid))
         return self.userHelper.toUserObj(dictUser=dictUser)
-    
+
     def getAdmin(self):
         return adminUser
-    
+
     def getapikeys(self):
         method = '/Auth/Keys'
         xEmbyAuth = {'X-Emby-Authorization': 'Token="{Token}"'.format(
@@ -97,14 +99,15 @@ class MediaServer(object):
 
     def logoutuser(self, userId):
         method = '/Sessions/Logout'
-        xEmbyAuth = {'X-Emby-Authorization': 'Emby UserId="{UserId}", Client="{Client}", Device="{Device}", DeviceId="{DeviceId}", Version="{Version}", Token="{Token}"'.format(
-            UserId=userId,
-            Client='account-automation',
-            Device=socket.gethostname(),
-            DeviceId=hash(socket.gethostname()),
-            Version=__version__,
-            Token= self.adminUser.AccessToken
-        )}
+        xEmbyAuth = {
+            'X-Emby-Authorization': 'Emby UserId="{UserId}", Client="{Client}", Device="{Device}", DeviceId="{DeviceId}", Version="{Version}", Token="{Token}"'.format(
+                UserId=userId,
+                Client='account-automation',
+                Device=socket.gethostname(),
+                DeviceId=hash(socket.gethostname()),
+                Version=__version__,
+                Token=self.adminUser.AccessToken
+            )}
         try:
             response = self.server_request(hdr=xEmbyAuth, method=method, data=None)
             self.adminUser.AccessToken = None
@@ -137,7 +140,8 @@ class MediaServer(object):
 
     def updateuserpolicy(self, user):
         if self.adminUser.AccessToken is None:
-            _log.error(__class__+'.updateuserpolicy requires an admins AccessToken before '+user.Name+' can be updated.')
+            _log.error(
+                __class__ + '.updateuserpolicy requires an admins AccessToken before ' + user.Name + ' can be updated.')
         method = '/Users/{Id}/Policy'.format(Id=user.Id)
         data = self.userHelper.todictPolicy(policyObj=user.Policy)
         try:
@@ -151,7 +155,8 @@ class MediaServer(object):
 
     def updateuserconfig(self, user):
         if self.adminUser.AccessToken is None:
-            _log.error(__class__+'.updateuserconfig requires an admins AccessToken before '+user.Name+' can be updated.')
+            _log.error(
+                __class__ + '.updateuserconfig requires an admins AccessToken before ' + user.Name + ' can be updated.')
         method = '/Users/{Id}/Configuration'.format(Id=user.Id)
         data = self.userHelper.todictConfig(configObj=user.Configuration)
         try:
@@ -165,7 +170,8 @@ class MediaServer(object):
 
     def updateuserpassword(self, AccessToken, userId, currentPw, newPw):
         if AccessToken is None:
-            _log.error(__class__+'.updateuserpassword requires an AccessToken before password update for User:'+userId)
+            _log.error(
+                __class__ + '.updateuserpassword requires an AccessToken before password update for User:' + userId)
 
         method = '/Users/{Id}/Password'.format(Id=userId)
         tokenHeader = {'X-Emby-Token': AccessToken}
@@ -180,11 +186,11 @@ class MediaServer(object):
             _log.critical(inst)
             _log.debug("Password update failed for user id: {user}".format(user=userId))
 
-    def getusers(self, IsHidden = str(''), IsDisabled = str(''), IsGuest = str('')):
-        method = '/Users?IsHidden={Hidden}&IsDisabled={Disabled}&IsGuest={Guest}'\
+    def getusers(self, IsHidden=str(''), IsDisabled=str(''), IsGuest=str('')):
+        method = '/Users?IsHidden={Hidden}&IsDisabled={Disabled}&IsGuest={Guest}' \
             .format(Hidden=IsHidden,
-                     Disabled=IsDisabled,
-                     Guest=IsGuest)
+                    Disabled=IsDisabled,
+                    Guest=IsGuest)
         try:
             dictUsers = self.server_getrequest(hdr=self.tokenHeader, method=method)
         except Exception as inst:
@@ -198,7 +204,7 @@ class MediaServer(object):
             serverusers.append(self.userHelper.toUserObj(dictUser=dictUser))
 
         return serverusers
-    
+
     def getlibraryinfo(self):
         info = {}
         method = '/Items/Counts'
@@ -211,10 +217,14 @@ class MediaServer(object):
             _log.critical(inst.args)
             _log.critical(inst)
             _log.debug("Cannot retrieve library counts from server: {server}".format(server=self.url))
-        
+
         return info
-    
-    def search(self, Name='', Ids='', FolderName='', Years='', IsPlayed='', Artists='', Albums='', Person='', Studios='', MinPremiereDate='', MaxPremiereDate='', MaxMPAARating='', MinCommunityRating='', MinCriticRating='', IsHD='', Is3D='', IsMissing='', IsUnaired='', Fields=[ParentId,DateCreated,SortName], ExcludeItemTypes='', SortBy='', SortOrder='Descending', Limit=''):
+
+    def search(self, Name='', Ids='', FolderName='', Years='', IsPlayed='', Artists='', Albums='', Person='',
+               Studios='', MinPremiereDate='', MaxPremiereDate='', MaxMPAARating='', MinCommunityRating='',
+               MinCriticRating='', IsHD='', Is3D='', IsMissing='', IsUnaired='',
+               Fields=[ParentId, DateCreated, SortName], ExcludeItemTypes='', SortBy='', SortOrder='Descending',
+               Limit=''):
         folderId = ''
         try:
             method = '/User/{UserID}/Items'
@@ -223,8 +233,8 @@ class MediaServer(object):
                 if f['Name'] == FolderName:
                     folderId = f['Id']
                     break
-            s=","
-            t="|"
+            s = ","
+            t = "|"
             method = '/User/{UserId}/Items?{name}&{ids}&{folderId}&{years}&{isPlayed}&{artists}&{albums}&{person}&{studios}&{minPremiereDate}&{maxPremiereDate}&{maxMPAARating}&{minCommRat}&{minCritRat}&{HD}&{threeD}&{missing}&{unaired}&{fields}&{exclude}&{sortBy}&{order}&{limit}'.format(
                 name="NameStartsWith=" + Name,
                 ids="Ids=" + s.join(Ids),
@@ -256,12 +266,12 @@ class MediaServer(object):
             _log.critical(inst.args)
             _log.critical(inst)
             _log.debug("Cannot retrieve search results of custom query from server: {server}".format(server=self.url))
-    
-    def customsql(self, query = str(''), usernamesNotIds = str('')):
+
+    def customsql(self, query=str(''), usernamesNotIds=str('')):
         method = '/user_usage_stats/submit_custom_query'
         data = {'CustomQueryString': '"{Query}"', 'ReplaceUserId': '"{replace}"'.format(
-            Query = query,
-            replace = usernamesNotIds
+            Query=query,
+            replace=usernamesNotIds
         )}
         try:
             results = self.server_getrequest(hdr=self.tokenHeader, method=method, data=data)
@@ -269,10 +279,11 @@ class MediaServer(object):
             _log.critical(type(inst))
             _log.critical(inst.args)
             _log.critical(inst)
-            _log.debug("Cannot retrieve results of custom query from server: {server} \nQuery: {customQuery}".format(server=self.url, customQuery=query))
-            
+            _log.debug("Cannot retrieve results of custom query from server: {server} \nQuery: {customQuery}".format(
+                server=self.url, customQuery=query))
+
         return results
-    
+
     def info(self):
         method = '/System/Info'
         try:
@@ -282,11 +293,11 @@ class MediaServer(object):
             _log.critical(inst.args)
             _log.critical(inst)
             _log.debug("Cannot get info for server: {server}".format(server=self.url))
-        
+
         return info
-    
+
     def ping(self):
-        info = self.info() # '/System/Ping' not working for some reason
+        info = self.info()  # '/System/Ping' not working for some reason
         if info:
             return True
         else:
@@ -299,107 +310,111 @@ class MediaServer(object):
             return True
         except exceptions as e:
             _log.critical(e)
-        
-   def shutdown(self):
-        method = '/System/Shutdown'
-        try:
-            response = self.server_request(hdr=self.tokenHeader, method=method)
-            return True
-        except exceptions as e:
-            return False
-        
-    def server_request(self, hdr, method, data=None):
-        hdr = {'accept': 'application/json', 'Content-Type': 'application/json', **hdr}
-        _log.critical(u"Method: {method}\nHeaders:\n{headers}\nData:\n{data}".format(
-            method = method,
-            headers = hdr,
-            data = data
+
+
+def shutdown(self):
+    method = '/System/Shutdown'
+    try:
+        response = self.server_request(hdr=self.tokenHeader, method=method)
+        return True
+    except exceptions as e:
+        return False
+
+
+def server_request(self, hdr, method, data=None):
+    hdr = {'accept': 'application/json', 'Content-Type': 'application/json', **hdr}
+    _log.critical(u"Method: {method}\nHeaders:\n{headers}\nData:\n{data}".format(
+        method=method,
+        headers=hdr,
+        data=data
+    ))
+    rsp = requests.post(self.url + method, headers=hdr, data=json.dumps(data))
+
+    if rsp.status_code == 400:
+        raise exceptions.JellyfinBadRequest(rsp.content)
+    if rsp.status_code == 401:
+        raise exceptions.JellyfinUnauthorized(rsp.content)
+    if rsp.status_code == 403:
+        raise exceptions.JellyfinForbidden(rsp.content)
+    if rsp.status_code == 404:
+        raise exceptions.JellyfinResourceNotFound(rsp.content)
+    if rsp.status_code >= 500 and rsp.status_code < 600:
+        raise exceptions.JellyfinServerError(rsp.content)
+    if rsp.status_code != 200 and rsp.status_code != 204:
+        raise exceptions.JellyfinException("Invalid HTTP status {code} for method {method}.".format(
+            code=rsp.status_code,
+            method=method
         ))
-        rsp = requests.post(self.url+method, headers=hdr, data=json.dumps(data))
+    if rsp.content != b'':
+        result = rsp.json()
+    else:
+        result = rsp.status_code
+    _ppresult = pprint.pformat(result)
+    _log.critical(u"Result:\n{result}".format(result=_ppresult))
+    return result
 
-        if rsp.status_code == 400:
-            raise exceptions.JellyfinBadRequest(rsp.content)
-        if rsp.status_code == 401:
-            raise exceptions.JellyfinUnauthorized(rsp.content)
-        if rsp.status_code == 403:
-            raise exceptions.JellyfinForbidden(rsp.content)
-        if rsp.status_code == 404:
-            raise exceptions.JellyfinResourceNotFound(rsp.content)
-        if rsp.status_code >= 500 and rsp.status_code < 600:
-            raise exceptions.JellyfinServerError(rsp.content)
-        if rsp.status_code != 200 and rsp.status_code != 204:
-            raise exceptions.JellyfinException("Invalid HTTP status {code} for method {method}.".format(
-                code=rsp.status_code,
-                method=method
-            ))
-        if rsp.content != b'':
-            result = rsp.json()
-        else:
-            result = rsp.status_code
-        _ppresult = pprint.pformat(result)
-        _log.critical(u"Result:\n{result}".format(result=_ppresult))
-        return result
 
-    def server_getrequest(self, hdr, method, data=None):
-        hdr = {'accept': 'application/json', **hdr}
-        _log.critical(u"Method: {method}\nHeaders:\n{headers}\nData:\n{data}".format(
-            method=method,
-            headers=hdr,
-            data=data
+def server_getrequest(self, hdr, method, data=None):
+    hdr = {'accept': 'application/json', **hdr}
+    _log.critical(u"Method: {method}\nHeaders:\n{headers}\nData:\n{data}".format(
+        method=method,
+        headers=hdr,
+        data=data
+    ))
+    rsp = requests.get(self.url + method, headers=hdr, data=json.dumps(data))
+
+    if rsp.status_code == 400:
+        raise exceptions.JellyfinBadRequest(rsp.content)
+    if rsp.status_code == 401:
+        raise exceptions.JellyfinUnauthorized(rsp.content)
+    if rsp.status_code == 403:
+        raise exceptions.JellyfinForbidden(rsp.content)
+    if rsp.status_code == 404:
+        raise exceptions.JellyfinResourceNotFound(rsp.content)
+    if rsp.status_code >= 500 and rsp.status_code < 600:
+        raise exceptions.JellyfinServerError(rsp.content)
+    if rsp.status_code != 200 and rsp.status_code != 204:
+        raise exceptions.JellyfinException("Invalid HTTP status {code} for method {method}.".format(
+            code=rsp.status_code,
+            method=method
         ))
-        rsp = requests.get(self.url + method, headers=hdr, data=json.dumps(data))
+    if rsp.content != b'':
+        result = rsp.json()
+    else:
+        result = rsp.status_code
+    _ppresult = pprint.pformat(result)
+    _log.critical(u"Result:\n{result}".format(result=_ppresult))
+    return result
 
-        if rsp.status_code == 400:
-            raise exceptions.JellyfinBadRequest(rsp.content)
-        if rsp.status_code == 401:
-            raise exceptions.JellyfinUnauthorized(rsp.content)
-        if rsp.status_code == 403:
-            raise exceptions.JellyfinForbidden(rsp.content)
-        if rsp.status_code == 404:
-            raise exceptions.JellyfinResourceNotFound(rsp.content)
-        if rsp.status_code >= 500 and rsp.status_code < 600:
-            raise exceptions.JellyfinServerError(rsp.content)
-        if rsp.status_code != 200 and rsp.status_code != 204:
-            raise exceptions.JellyfinException("Invalid HTTP status {code} for method {method}.".format(
-                code=rsp.status_code,
-                method=method
-            ))
-        if rsp.content != b'':
-            result = rsp.json()
-        else:
-            result = rsp.status_code
-        _ppresult = pprint.pformat(result)
-        _log.critical(u"Result:\n{result}".format(result=_ppresult))
-        return result
 
-    def server_delete(self, hdr, method):
-        hdr = {'accept': '*/*', **hdr}
-        _log.critical(u"Method: {method}\nHeaders:\n{headers}".format(
-            method=method,
-            headers=hdr
+def server_delete(self, hdr, method):
+    hdr = {'accept': '*/*', **hdr}
+    _log.critical(u"Method: {method}\nHeaders:\n{headers}".format(
+        method=method,
+        headers=hdr
+    ))
+
+    rsp = requests.delete(url=self.url + method, headers=hdr)
+
+    if rsp.status_code == 400:
+        raise exceptions.JellyfinBadRequest(rsp.content)
+    if rsp.status_code == 401:
+        raise exceptions.JellyfinUnauthorized(rsp.content)
+    if rsp.status_code == 403:
+        raise exceptions.JellyfinForbidden(rsp.content)
+    if rsp.status_code == 404:
+        raise exceptions.JellyfinResourceNotFound(rsp.content)
+    if rsp.status_code >= 500 and rsp.status_code < 600:
+        raise exceptions.JellyfinServerError(rsp.content)
+    if rsp.status_code != 200 and rsp.status_code != 204:
+        raise exceptions.JellyfinException("Invalid HTTP status {code} for method {method}.".format(
+            code=rsp.status_code,
+            method=method
         ))
-
-        rsp = requests.delete(url=self.url+method, headers=hdr)
-
-        if rsp.status_code == 400:
-            raise exceptions.JellyfinBadRequest(rsp.content)
-        if rsp.status_code == 401:
-            raise exceptions.JellyfinUnauthorized(rsp.content)
-        if rsp.status_code == 403:
-            raise exceptions.JellyfinForbidden(rsp.content)
-        if rsp.status_code == 404:
-            raise exceptions.JellyfinResourceNotFound(rsp.content)
-        if rsp.status_code >= 500 and rsp.status_code < 600:
-            raise exceptions.JellyfinServerError(rsp.content)
-        if rsp.status_code != 200 and rsp.status_code != 204:
-            raise exceptions.JellyfinException("Invalid HTTP status {code} for method {method}.".format(
-                code=rsp.status_code,
-                method=method
-            ))
-        if rsp.content != b'':
-            result = rsp.json()
-        else:
-            result = rsp.status_code
-        _ppresult = pprint.pformat(result)
-        _log.critical(u"Result:\n{result}".format(result=_ppresult))
-        return result
+    if rsp.content != b'':
+        result = rsp.json()
+    else:
+        result = rsp.status_code
+    _ppresult = pprint.pformat(result)
+    _log.critical(u"Result:\n{result}".format(result=_ppresult))
+    return result
